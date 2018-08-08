64b7f3474f39df34224102e8c28deb65c080280b
Matt Laporte <matt.l.laporte@gmail.com>
2018-08-08 17:50:27 -0500

    kuramNetwork is fixed; the problem was with the numerical integration. In kuramotoNetworkParameterTest, added network regeneration repetitions and per-network repetitions. Generation of K for each N based on a function for more flexibility. Keep all generated networks in a cell array. Starting to convert the script into a function; this will not be very useful for huge combinations as it will take forever to run, though there could be a save-load function to bypass repeated calls.

--- kuramNetwork.m
+++ kuramNetwork.m
@@ -1,43 +1,61 @@
-%% Run Kuramoto oscillator through network
-function [theta, z] = kuramNetwork (net, lam, omega, theta0, steps, h)
-
+function [theta, z] = kuramNetwork(net, lam, omega, theta0, steps, h)
 % This script  runs a kuramoto oscillator through a pre-generated network
 % conditioned on local connections
 % Numerical integration through fourth-order Runge-Kutta Method
 % BC/ML/SWoNS/2018
 % Code adapted from appmath.wordpress.com, courtesy
 % Jeongho Kim, Mathematical Sciences, Seoul National University.
-% Someone please cleanv
 
 % network parameters
 N = net.numnodes;
+E = weightedA(net);  % transposed because of `dot` in kuramoto
 
-% allocate and set initial conditions
+% pre-allocate arrays and set initial conditions
+z = zeros(length(steps-1), 1);
 theta = zeros(N, steps);
 theta(:, 1) = theta0;
-z = zeros(steps, 1);
-z(1) = sum(exp(1i*theta(:, 1))) / N;
 
-for k = 1:steps-1    
-    thetaPairwiseDiffs = theta(:,k) - theta(:,k)'; %Generates phase matrix\
-    A = adjacency(net);
-    thetaPairwiseDiffs(~A) = 0; %no connection == 0
+% partial function for evaluating the numerical integral
+kuramotoPartial = @(thetaDiffs, varargin) kuramoto(thetaDiffs, omega, lam, N, varargin{:});
 
-    f1 = kuramoto (thetaPairwiseDiffs,lam,N,omega);
-    f2=kuramoto(thetaPairwiseDiffs+0.5*h*f1(:,1),lam,N,omega);
-    f3=kuramoto(thetaPairwiseDiffs+0.5*h*f2(:,1),lam,N,omega);           %4-th order Runge-Kutta method.
-    f4=kuramoto(thetaPairwiseDiffs+h*f3(:,1),lam,N,omega);        
-    theta(:,k+1) = theta(:,k)+(h/6)*(f1(:,1))+2*f2(:,1)+2*f3(:,1)+f4(:,1);
-
-    theta(:, k+1) = wrapTo2Pi(theta(:, k+1));
-
-    z(k+1) = sum(exp(1i*theta(:,k+1)))/N;
+for iter = 1:steps-1
+    % pairwise inter-node phase differences
+    thetaPairwiseDiffs = theta(:, iter) - theta(:, iter)'; 
+    
+    % numerical integration step
+    plusNoise = thetaPairwiseDiffs;% + rand(N)*0.1;
+    dtheta = rk4Step(kuramotoPartial, plusNoise, h, ones(N));
+    theta(:, iter+1) = theta(:, iter) + dtheta;
+    
+    % keep theta in [0, 2*pi)
+    theta(:, iter+1) = wrapTo2Pi(theta(:, iter+1));
+    
+    % network mean phase vector
+    z(iter+1) = sum(exp(1i*theta(:, iter+1))) / N;  
 end
 
-function f=kuramoto(x,lam,N,omega)
- 
-f=omega+(lam/N)*sum(sin(x))'; %Take out rand for noise
- 
+function A_w = weightedA(net)
+    % weighted adjacency table
+    weightedEdges = table2array(net.Edges);
+    % convert to matrix
+    A_w = zeros(net.numnodes);
+    for e = 1:size(weightedEdges, 1)
+        A_w(weightedEdges(e, 1), weightedEdges(e, 2)) = weightedEdges(e, 3);
+    end
 end
 
+function dTh = kuramoto(x, omega, lam, N, varargin)
+    % Kuramoto oscillator differential equation 
+    E = varargin{1};
+    dTh = omega + (lam / N) * dot(E, sin(x))';
+end
+
+function res = rk4Step(dxdt, x, h, varargin)
+    % 4-th order Runge-Kutta method on non-zero values in x
+    f1 = dxdt(x, varargin{:});
+    f2 = dxdt(x + 0.5*h*f1(:, 1), varargin{:});
+    f3 = dxdt(x + 0.5*h*f2(:, 1), varargin{:});
+    f4 = dxdt(x + h*f3(:, 1), varargin{:});        
+    res = (h/6)*f1(:, 1) + 2*f2(:, 1) + 2*f3(:, 1) + f4(:, 1);
+end
 end
